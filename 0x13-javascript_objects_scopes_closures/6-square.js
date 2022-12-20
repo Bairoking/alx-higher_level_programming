975d65dfa1fdfe12215cff50f90515365888e6a8
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-12-20 12:54:31 -0800

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Square class that inherits from previous square class
+
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let sanna = 0; sanna < this.height; sanna++) console.log(c.repeat(this.width));
+    }
+  }
+};
