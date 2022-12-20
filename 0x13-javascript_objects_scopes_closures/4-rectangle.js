2243632eceaf15cc7fad90c9c31ba8e847c4812d
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-12-20 12:39:22 -0800

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Rotate and Double the width and height the Rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    for (let sanna = 0; sanna < this.height; sanna++) console.log('X'.repeat(this.width));
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};
